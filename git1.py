1d789ba3e89a812acf05cafc207e2831f0add5d9
Dallin Post <ijiff08@Dallins-MacBook-Air-94.local>
2024-08-01 13:13:40 -0400

    added array function

--- git1.py
+++ git1.py
@@ -4,8 +4,12 @@ def math():
     z = x * y - 5
     return z
 
+def arrays():
+    my_array = [i for i in range(10)]
+    return my_array
 
 if __name__ == '__main__':
     name = 'Dallin'
     print(f'Your name is {name}')
     print(math())
+    print(arrays())
