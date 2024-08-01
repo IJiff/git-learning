ff0f8c3f4a464cf72dacf665370e83dcc54f9680
Dallin Post <ijiff08@Dallins-MacBook-Air-94.local>
2024-08-01 13:08:30 -0400

    added math

--- git1.py
+++ git1.py
@@ -1,6 +1,11 @@
-name = 'Dallin'
-print(f'Your name is {name}')
+def math():
+    x = 12
+    y = 24
+    z = x * y - 5
+    return z
+
 
 if __name__ == '__main__':
     name = 'Dallin'
     print(f'Your name is {name}')
+    print(math())
